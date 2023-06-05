662e09002a365570420af96677bb7ac852a4a2bd
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-06-04 21:49:15 -0700

    Update bag_dataset.py

--- bag_dataset.py
+++ bag_dataset.py
@@ -2,9 +2,9 @@
 Coverts a rosbag into a dataset for Perspective Transorming VAE(s)
 '''
 import os
-import time
-import argparse
 import rosbag
+import argparse
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 _help = "path to previously learned cartographer map (*.pbstream)"
@@ -16,13 +16,12 @@ args = parser.parse_args()
 if __name__ == "__main__":
   # run a bag in offline localization-only mode (requires a previously learned SLAM map)
   os.system("rosparam use_sim_time true")
+  # create copy of filtered /tf so they don't accumulate & conflict in the new bag
+  os.system("rosbag filter " + args.bag_file + " /tmp/filtered.bag 'topic != \"/tf\"'")
   # carl_localize.launch expects the map to be here
   os.system("cp " + args.map_file + " /tmp/current.pbstream")
   os.system("roslaunch cartographer_toyota_hsr carl_localize.launch &")
   os.system("rosrun map_server map_saver --occ 49 --free 40 -f '/tmp/map.pgm'")
-  
-  # create copy of filtered /tf so they don't accumulate & conflict in the new bag
-  os.system("rosbag filter " + args.bag_file + " /tmp/filtered.bag 'topic != \"/tf\"'")
   
   # weird glitch where earliest /tf messages aren't captured by catrographer
   for i in range(10):
@@ -33,23 +32,41 @@ if __name__ == "__main__":
   os.system("rosbag play --clock --rate 2.5 /tmp/filtered.bag")
   os.system("rosnode kill /loc_bag")
   
-  # print("sleeping for a bit to ensure cartographer has enough time to finish")
-  # finish_traj & save pbs & pgm 
+  # load the picture of the map
+  map_img = None
+  with open("/tmp/map.pgm", 'rb') as pgmf:
+    map_img = plt.imread(pgmf)
+  map_plot = plt.imshow(map_img)
+  plt.show()
   
-  # load map.pgm from sys.argv
-  # load rosbag from sys.argv
-  bag = rosbag.Bag('/tmp/test.bag')
-  for topic, msg, t in bag.read_messages(topics=['/tf', 'image_proc_resize/image']):
+  # load rosbag & get a path of length non-significant length
+  bag = rosbag.Bag('/tmp/loc.bag')
+  path_x, path_y = [], []
+  for topic, msg, t in bag.read_messages(topics=['/tf']):
     print(msg)
+    x,y = 0,0
+    path_x.append(x)
+    path_y.append(y)
   bag.close()
-  # get a path of length X
+  
   # overlay this path on the map 
-  # use keys to translate & rotate the path
+  plt.scatter(x=path_x, y=path_y, c='r', s=40)
+  # use keys to translate, rotate, & scale the path
   # add buffer to map to enable rotate + crop
   # crop with robot position at center
   # rotate, then crop to final size
   # draw robot's pose (start simple, like an arrow)
+  
   # restart and loop over the whole bag
+  bag = rosbag.Bag('/tmp/loc.bag')
+  path_x, path_y = [], []
+  for topic, msg, t in bag.read_messages(topics=['/tf']):
+    print(msg)
+    x,y = 0,0
+    path_x.append(x)
+    path_y.append(y)
+  bag.close()
+  
   # save in the format Tim's already using (i.e. csv)
   # save cropped image of map & resized camera image
   
