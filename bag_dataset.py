7e614c9b71fe181c86e43eafbd67d1fb6368d328
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-06-04 20:46:05 -0700

    Update bag_dataset.py

--- bag_dataset.py
+++ bag_dataset.py
@@ -1,16 +1,28 @@
 '''
 Coverts a rosbag into a dataset for Perspective Transorming VAE(s)
 '''
+import os
+import argparse
 import rosbag
+
+parser = argparse.ArgumentParser()
+_help = "path to previously learned cartographer map (*.pbstream)"
+parser.add_argument("map_file", type=str, required=True, help=_help)
+args = parser.parse_args()
 
 if __name__ == "__main__":
   # run a bag in offline localization-only mode (requires a previously learned SLAM map)
+  # carl_localize.launch expects the map to be here
+  os.system("cp " + args.map_file + " /tmp/current.pbstream")
+  os.system("roslaunch cartographer_toyota_hsr carl_localize.launch &")
+  
   # save a new bag with robot's pose & the FPV camera images
+  os.system("rosbag record /tf /image_proc_resize/image")
   
   # load map.pgm from sys.argv
   # load rosbag from sys.argv
   bag = rosbag.Bag('/tmp/test.bag')
-  for topic, msg, t in bag.read_messages(topics=['image_proc_resize/image', '/tf']):
+  for topic, msg, t in bag.read_messages(topics=['/tf', 'image_proc_resize/image']):
     print(msg)
   bag.close()
   # get a path of length X
