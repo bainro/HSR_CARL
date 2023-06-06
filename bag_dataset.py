abf50ba78f3fb5669cd0d6aaa383e9f13760db45
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-06-05 18:12:53 -0700

    Update bag_dataset.py

--- bag_dataset.py
+++ bag_dataset.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
 
   os.system("roslaunch cartographer_toyota_hsr carl_localize.launch &")
   time.sleep(3)
-  os.system("rosservice call /trajectory_query 'trajectory_id: 1' > /tmp/robot_traj.txt")
+  os.system("rosservice call /trajectory_query 'trajectory_id: 1' > /tmp/traj.txt")
   time.sleep(3)
   os.system("pkill cart")
   # get just the pose position (x,y) and the corresponding timestamp (secs)
