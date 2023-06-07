25f5215feb668c0efc1fd7160ec32e9944006b05
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-06-07 02:45:41 -0700

    Update bag_dataset.py

--- bag_dataset.py
+++ bag_dataset.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
   # print("assumes HxWxC image format!")
   rot_w = int((map_img.shape[1] * roi_rel_w) // 1)
   
-  plt.figure(figsize=(12, 32))
+  plt.figure(figsize=(7, 32))
   
   for c, i in enumerate(range(len(trans_path_x))):
     fpv_img = np.zeros(shape=(rot_w, rot_w, 3))
@@ -278,12 +278,12 @@ if __name__ == "__main__":
     
     if c > 15: break
     ax = plt.subplot(5,4,c+1)
+    ax.axis('off')
     ax.imshow(fpv_img)
   
   ax1 = plt.subplot(5,1,5)
   ax1.imshow(map_img)
   ax1.scatter(x=trans_path_x, y=trans_path_y, c='b', s=3)
-  plt.axis('off')
   fig.savefig('/tmp/test2.svg', format='svg', dpi=1200)
   plt.show()
   
