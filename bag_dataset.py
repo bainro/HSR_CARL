dc17323506f05e6f21e078af062a278f09d26f52
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-06-07 18:21:58 -0700

    Update bag_dataset.py

--- bag_dataset.py
+++ bag_dataset.py
@@ -339,11 +339,7 @@ if __name__ == "__main__":
       if msg_t < path_secs[i]:
         continue
       assert msg.width > msg.height, "image width must be greater than image height"
-      
-      print("type(msg.data[0]): ", type(msg.data[0]))
-      print("msg.data[0]: ", msg.data[0])
-      print("list(msg.data)[0]: ", list(msg.data)[0])
-      cam_img = np.asarray(list(msg.data), dtype=np.float33)
+      cam_img = np.asarray(list(msg.data), dtype=np.float32)
       cam_img = cam_img.reshape((msg.height, msg.width, 3))
       # crop to center
       x_offset = int((msg.width - msg.height) // 2)
