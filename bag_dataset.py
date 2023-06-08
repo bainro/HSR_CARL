80e0f91edf1226b45571e9d75bebb4884f242c13
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-06-07 18:00:00 -0700

    Update bag_dataset.py

--- bag_dataset.py
+++ bag_dataset.py
@@ -304,12 +304,12 @@ if __name__ == "__main__":
     print("xpo: ", xpo)
     print("x_start: ", x_start)
     print("x_end: ", x_end)
-    if xpo != 0: # literal edge case
+    if xpo != 0: # literal edge (of map) case
       if len(rot_map.shape) == 3: # e.g. RGB
         gmp_img[:y_end-y_start, xpo:, :] = rot_map[y_start:y_end, x_start:x_end, :]
       else: # e.g. grayscale
-        gmp_img[:y_end-y_start, xpo+1:] = rot_map[y_start:y_end, x_start:x_end]
-    elif ypo != 0: # literal edge case
+        gmp_img[:y_end-y_start, xpo+1:] = rot_map[y_start:y_end, x_start:x_end+1]
+    elif ypo != 0: # literal edge (of map) case
       if len(rot_map.shape) == 3: # e.g. RGB
         gmp_img[ypo:, :x_end-x_start, :] = rot_map[y_start:y_end, x_start:x_end, :]
       else: # e.g. grayscale
