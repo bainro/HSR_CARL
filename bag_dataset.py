c71ad384754a2f9e3ba10d751cf11149df5594ad
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-06-09 18:08:03 -0700

    Update bag_dataset.py

--- bag_dataset.py
+++ bag_dataset.py
@@ -141,10 +141,10 @@ if __name__ == "__main__":
       return scale_or_rot, path_v
     return ky_cb
   
-  up_cb = key_cb_gen(0.15, -0.1)
-  down_cb = key_cb_gen(-0.15, 0.1)
-  left_cb = key_cb_gen(0.003, -0.1)
-  right_cb = key_cb_gen(-0.003, 0.1)        
+  up_cb = key_cb_gen(-0.15, -0.1)
+  down_cb = key_cb_gen(0.15, 0.1)
+  left_cb = key_cb_gen(-0.003, -0.1)
+  right_cb = key_cb_gen(0.003, 0.1)        
   
   shift_on = False
   enter_pressed = False
