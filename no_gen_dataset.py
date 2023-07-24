b309457485dfb9422567c5e21b67252cdc1b5919
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-07-24 10:39:33 -0700

    Update no_gen_dataset.py

--- no_gen_dataset.py
+++ no_gen_dataset.py
@@ -265,7 +265,8 @@ if __name__ == "__main__":
     _ = os.system(f"rm -fr {args.out_dir} > /dev/null 2>&1")
     os.makedirs(args.out_dir, exist_ok=True)
       
-  for c, i in enumerate(range(len(trans_path_x))):
+  for i in range(len(trans_path_x)):
+    print(i)
     if len(map_img.shape) == 3: # e.g. RGB
       gmp_img = np.zeros(shape=(gmp_w, gmp_w, 3))
     else:
